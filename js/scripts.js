c52fe06d645550c1b6e61b328f4fe74f29ad1b6a
jakeportaro <jakeportaro@gmail.com>
2022-07-25 12:56:58 -0700

    created pokemon list

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  { name: 'Charizard', height: 10, type: ['fire','hot','lava']},
+  { name: 'picachu', height: 1, type: ['electric','electricity','zap']},
+  { name: 'snorlax', height: 50, type: ['fat','large','giant']}
+];
